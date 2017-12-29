87248f46c751073aca419790c7e8c108a96d3850
Philipp Mandler <info@philipp-mandler.com>
2017-12-30 00:29:34 +0100

    box2d: add hash

--- Formula/box2d.rb
+++ Formula/box2d.rb
@@ -7,6 +7,7 @@ class Box2d < Formula
 
     bottle do
       cellar :any
+      sha256 "75d62738b13d2836cd56647581b6e574d4005a6e077ddefa5d727d445d649752" => :sierra
       sha256 "967e9f17c6133f253be1be4f845899160af7c8cc8d072e41b176d9c636911a19" => :el_capitan
       sha256 "1559be0d79e66e8074a051f36db5bbf21a9d556013803cce7e04184bd28028a9" => :yosemite
       sha256 "40effd7d4952d37d15e7b34da4b9c207956b1d79bdd856f9f03307ecf52a5b3f" => :mavericks
